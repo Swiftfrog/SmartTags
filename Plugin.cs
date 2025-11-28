f7097907c5157e762da3c470d7709defc9a3fb7f
albert <swiftfrog@gmail.com>
2025-11-28 22:57:51 +0800

    Plugin?

--- Plugin.cs
+++ Plugin.cs
@@ -15,7 +15,7 @@ public class Plugin : BasePluginSimpleUI<SmartTagsConfig>
     public override string Description => "基于 TMDB 数据自动管理原产地、年代及 IMDb Top 250 标签。";
     public override Guid Id => new Guid("AA721234-B111-4222-A333-123456789000");
 
-    public static Plugin Instance { get; private set; }
+    public static Plugin? Instance { get; private set; }
     
     public SmartTagsConfig Configuration => GetOptions();
 
